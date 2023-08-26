8b93fe98ec04858c3d825cb12206052a76fc662e
joaovq <vitorsnta90@gmail.com>
2023-08-25 22:45:02 -0300

    test(app): update category type field for char

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    categoryName = models.TextField(max_length=30)
+    categoryName = models.CharField(max_length=65)
     
     def __str__(self) -> str:
-        return f"{self.id}-{self.categoryName}"
+        return self.categoryName
     
 # Create your models here.
 class Recipes(models.Model):
@@ -20,12 +20,12 @@ class Recipes(models.Model):
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     is_published = models.BooleanField(default=False)
-    servings_unit = models.TextField(max_length=65)
+    servings_unit = models.CharField(max_length=65)
     created_at = models.DateTimeField(auto_created=True,)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(to=Category, on_delete=models.SET_NULL, null=True, blank=True, default=None) 
     author = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True) 
     cover_image = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     def __str__(self) -> str:
-        return self.name
+        return self.title
     
