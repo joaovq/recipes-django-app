34e06ee1c73c319b79f845097604a004e1559d8d
joaovq <joaovqdev@gmail.com>
2023-08-31 11:42:37 -0300

    feature(app): implements pagination in app recipes

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ app_name = 'app'
 
 urlpatterns = [
     path('',views.home_view, name='home'),
+    path('recipes/search/',view=views.search_recipes_view, name='search_recipes'),
     path('recipes/<int:recipe_id>/',views.recipe_details_view, name='recipe_details'),
     path('recipes/category/<int:category_id>/',views.categories_recipes_view, name='category_recipes'),
 ]
