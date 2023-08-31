1e0977695a7f90a71f2376a1897dfc3876c0e2a4
joaovq <joaovqdev@gmail.com>
2023-08-31 11:41:53 -0300

    feature(app): implements pagination in app recipes

--- app/views.py
+++ app/views.py
@@ -1,16 +1,20 @@
-from datetime import date
-from django.http import Http404, HttpResponse
+from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from app.models import Category, Recipes
+from django.db.models import Q
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination
 
 
 def home_view(request):
     recipes = Recipes.objects.filter(is_published=True).order_by('-id')
+    page_obj, pagination_range = make_pagination(request=request,queryset=recipes)
     return render(
         request=request,
         template_name='recipes/pages/home.html',
         context={
-            'recipes': recipes,
+            'recipes': page_obj,
+            'pagination_range':pagination_range,
             'is_detail_page': False
         }
     )
@@ -36,11 +40,34 @@ def recipe_details_view(request, recipe_id):
 
 
 def categories_recipes_view(request, category_id):
-    recipes = get_list_or_404(Recipes.objects.filter(category__id=category_id, is_published=True))
-    category = get_object_or_404(Category,pk=category_id)
+    recipes = get_list_or_404(Recipes.objects.filter(
+        category__id=category_id, is_published=True))
+    category = get_object_or_404(Category, pk=category_id)
     return render(request=request, template_name='recipes/pages/home.html', context={
         'recipes': recipes,
         'is_detail_page': False,
         'category': category,
         'title': f"{category.categoryName} | Recipes"
+    })
+
+
+def search_recipes_view(request):
+    search_term = request.GET.get('search', '').strip()
+    if not search_term:
+        raise Http404
+    recipes = Recipes.objects.filter(
+        Q(
+          Q(title__icontains=search_term) |
+          Q(description__icontains=search_term)
+        ),
+        is_published=True
+    ).order_by('-id')
+    page_obj, pagination_range = make_pagination(request=request,queryset=recipes)
+    return render(request=request, template_name='recipes/pages/recipes_search.html', context={
+        'recipes': page_obj,
+        'is_detail_page': False,
+        'title': f"Search Results for \"{search_term}\"",
+        'search_term': search_term,
+        'pagination_range':pagination_range,
+        'additional_url_query':f'&search={search_term}'  
     })
---
+++ utils/pagination.py
@@ -0,0 +1,56 @@
+import math
+from django.core.paginator import Paginator
+
+
+def make_pagination_range(
+    page_range,
+    qt_pages,
+    current_page
+):
+    middle_range = math.ceil(qt_pages/2)
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+    total_pages = len(page_range)
+    
+    if current_page > total_pages:
+        raise PageError
+
+    if start_range < 0:
+        start_range = 0
+        stop_range += start_range_offset 
+        
+    if stop_range >= total_pages:
+        start_range = start_range - abs(total_pages - stop_range)            
+        
+    pagination = page_range[start_range:stop_range]
+    return {
+        'pagination': pagination,
+        'qt_pages': qt_pages,
+        'page_range':page_range,
+        'current_page': current_page,
+        'total_pages':total_pages,
+        'start_range':start_range,
+        'stop_range':stop_range,
+        'first_page_out_of_range': current_page > middle_range,
+        'last_page_out_of_range':stop_range < total_pages
+    }
+
+def make_pagination(request, queryset, qt_pages = 4, per_page = 10):
+    try:
+        current_page = int(request.GET.get('page',1))
+    except ValueError:
+        current_page = 1
+    paginator = Paginator(queryset, per_page)
+    
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        qt_pages,
+        current_page
+    )
+    page_obj = paginator.get_page(current_page)
+    return page_obj,pagination_range
+    
+
+class PageError(Exception):
+    pass
